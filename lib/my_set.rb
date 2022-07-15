feac00c8b3905d54f8fd499137c79dd6fd693715
Will <willchesson@gmail.com>
2022-07-15 15:30:45 -0400

    commit tree 5e43fb4499f3fcb8ec37874812d9698e3d9d405a

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,25 @@
 # your code here
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each{|value| @hash[value] = true}
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true      #add a value as a key on the has
+        self                     #return the updated set
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+end
